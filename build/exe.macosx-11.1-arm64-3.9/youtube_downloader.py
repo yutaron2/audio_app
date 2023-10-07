95b0556c7bf95dcc26236c97ff591c2a235f5f0b
yutaron2 <awaji.re46893rgy@gmail.com>
2023-10-08 02:25:37 +0900

    3機能対応

--- build/exe.macosx-11.1-arm64-3.9/youtube_downloader.py
+++ build/exe.macosx-11.1-arm64-3.9/youtube_downloader.py
@@ -1,22 +1,42 @@
 import tkinter as tk
+from pytube import YouTube
+import threading
 
-class AudioPlayerApp(tk.Tk):
+class YoutubeDownloaderApp(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.title("Audio Player")
-        self.geometry("300x100")
+        self.title("Youtube Downloader")
+        self.geometry("500x200")
 
-        play_button = tk.Button(self, text="Play", command=self.play_audio)
-        play_button.pack(pady=10)
+        self.url_entry = tk.Entry(self, width=50)
+        self.url_entry.pack(pady=10)
 
-        stop_button = tk.Button(self, text="Stop", command=self.stop_audio)
-        stop_button.pack(pady=10)
+        download_button = tk.Button(self, text="Download", command=self.start_download_thread)
+        download_button.pack(pady=10)
 
-    def play_audio(self):
-        # ここに音声を再生するコードを書く
-        pass
+        self.status_label = tk.Label(self, text="")
+        self.status_label.pack(pady=10)
 
-    def stop_audio(self):
-        # ここに音声を停止するコードを書く
-        pass
+    def start_download_thread(self):
+        # ダウンロード処理を別スレッドで実行
+        threading.Thread(target=self.download_video).start()
+
+    def download_video(self):
+        url = self.url_entry.get()
+        if not url:
+            self.status_label.config(text="Please enter a URL")
+            return
+
+        try:
+            self.status_label.config(text="Downloading...")
+            yt = YouTube(url)
+            stream = yt.streams.get_highest_resolution()
+            stream.download()
+            self.status_label.config(text="Download complete!")
+        except Exception as e:
+            self.status_label.config(text=f"Error: {str(e)}")
+
+if __name__ == "__main__":
+    app = YoutubeDownloaderApp()
+    app.mainloop()
--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
-    "packages": ["os", "tkinter", "moviepy"],
+    "packages": ["os", "tkinter", "pytube"],  # "pytube" を追加
     "excludes": ["tkinter.ttk", "tkinter.tix", "tkinter.scrolledtext"],
     "include_files": ["audio_extractor.py", "youtube_downloader.py", "audio_player.py", "helpers/"]
 }
@@ -10,9 +9,9 @@ build_exe_options = {
 base = None
 
 setup(
-    name = "MyApp",
-    version = "0.1",
-    description = "My Application",
-    options = {"build_exe": build_exe_options},
-    executables = [Executable("main.py", base=base)]
+    name="MyApp",
+    version="0.1",
+    description="My Application",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)]
 )
